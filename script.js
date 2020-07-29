d6e7b4f86ca8ecdb269d3249f513c7d70e9ec93b
joekazem <62042681+joekazem@users.noreply.github.com>
2020-07-28 23:56:49 -0400

    this commit has the working 5 day forcast

--- script.js
+++ script.js
@@ -13,42 +13,52 @@ function setQuery() {
     const queryUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + searchBox + "&units=imperial&APPID=39ce06798e8547b26b3493c8a7b2b944";
     $.ajax({
         url: queryUrl,
-        method: "GET", 
+        method: "GET",
         success: function (response) {
-        $("#temp").text(response.main.temp)
-        $("#humid").text("Humidity " + response.main.humidity + "%")
-        $("#wind").text("wind Speed " + response.wind.speed + "Mph") //append to the html document
-        //var uv1 = $("<h3>")
-        console.log(response);
-        console.log(response.weather[0].description);
-    }
+            $("#temp").text(response.main.temp)
+            $("#humid").text("Humidity " + response.main.humidity + "%")
+            $("#wind").text("wind Speed " + response.wind.speed + "Mph") //append to the html document
+            //var uv1 = $("<h3>")
+            //console.log(response.coord);
+            //console.log(response.weather[0].description);
+            fiveDay(response.coord.lat, response.coord.lon);
+        }
     })
 
     //save the city searching for in local storage
 
     //call function that creates the html for the saved cities
-    if (evt.keycode == 13) {
-        getResults(searchBox.value);
-        console.log(searchBox.value);
-    }
-  
+    //   if (evt.keycode == 13) {
+    //      getResults(searchBox.value);
+    //    console.log(searchBox.value);
+    // }
 }
 
-function fiveDayForecast () {
-    const searchBox = document.querySelector('#search-box').value;
-    const fiveDayApi = "https://api.openweathermap.org/data/2.5/forecast?q=" + searchBox + "&appid=39ce06798e8547b26b3493c8a7b2b944";
-    console.log(fiveDayApi);
+function fiveDay(lat, lon) {
+    const fiveDayApi = 'https://api.openweathermap.org/data/2.5/onecall?lat=' + lat + '&lon=' + lon + '&exclude=current,minutely,hourly&units=imperial&appid=39ce06798e8547b26b3493c8a7b2b944';
+    var fiveDayData = []
     $.ajax({
-            url: fiveDayApi,
-            method: "GET",
-            success: function (data) {
-                console.log (data);
-            },
-            error: function (err) {
-                console.log (err);
+        url: fiveDayApi,
+        method: "GET",
+        success: function (data) {
+            console.log(data.daily[0].temp.day);
+            for (let i = 1; i < 6; i++) {
+                console.log("data", data.daily);
+                fiveDayData.push(data.daily[i])
+                $(`#date${i}`).text(data.daily[i].dt)
+                $(`#temp${i}`).text(data.daily[i].temp.max)
+                 $(`#humidity${i}`).text(data.daily[i].humidity)
+                 $(`#press${i}`).text(data.daily[i].pressure)
+                $(`#uv${i}`).text(data.daily[i].uvi)
             }
-            
-    })
+             
+            return data
+ 
+        },
+        error: function (err) {
+            console.log(err);
+        }
        
+
+    })
 }
-    fiveDayForecast ();  
